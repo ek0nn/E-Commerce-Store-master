bac3164917494cd34a5ebc8a97e54b7bafb51d34
ek0nn <msadhak123@hotmail.com>
2023-03-23 23:26:03 +0000

    FILTER WORKS YAY

--- src/item.js
+++ src/item.js
@@ -7,132 +7,156 @@ import cp3 from "./img/CP3 Mechanical Gaming Keyboard .jpg"
 import ducky from "./img/ducky.jpg"
 import emi from "./img/EMINENCESWITCH.jpg"
 import nomadkc from "./img/nomadkeycaps.jpg"
+import gmkam from "./img/GMKAmethyst.jpg"
+import gmkevil from "./img/gmkevildolch.jpg"
+import gmkrain from "./img/gmkrainyday.png"
+import kailhp from "./img/kailhproswitches.jpg"
+import cable from "./img/konocoiledergodoxcable.jpg"
+import cable2 from "./img/konogx16coiledaviatorcables.jpg"
+import sushimat from "./img/SushiMat.jpg"
+import disc from "./img/disck2.jpg"
+import penguin from "./img/penguinswitch.png"
+import starwars from "./img/starwars.png"
+
 export const ITEMS =      [
     {
     id: '1',
+    keyboard:"1",
     name: "Akko ACR 61 ",
-    price: 80,
+    price: 80.00,
     productImg: Akko1,
     pDesc: " This model comes with a translucent case made with high-quality acrylic material. This form factor could be a good presence for RGB keyboard, lights could shine through the case but not too dazzling since the case is with a well matted texture. Extra set of ASA profile PBT keycaps are included as a gift, you have more choices of make your own tone of a pink keyboard"
 
 },
 { 
     id: '2',
+    keyboard:"1",
     name: "Akko ACR Blue",
-    price: 20,
+    price: 20.00,
     productImg: AkkoBlue,
 
 },
 { 
     id: '3',
     name: "Cherry MX Black",
-    price: 10,
+    price: 10.00,
     productImg: blackmx,
+    type: "99",
 
 },
 { 
     id: '4',
     name: "CP3 Mechanical Gaming Keyboard",
-    price: 10,
+    price: 10.00,
     productImg: cp3,
 
 },
 { 
     id: '5',
     name: "Sappihre Keycaps",
-    price: 10,
+    price: 10.00,
     productImg: bluekc,
+    type: "99",
+
 
 },
 { 
     id: '6',
     name: "Ducky one 2 mini",
-    price: 10,
-    productImg: ducky
+    price: 10.00,
+    productImg: ducky,
+
 
 },
 { 
     id: '7',
     name: "Emi Shadows",
-    price: 10,
-    productImg: emi
+    price: 10.00,
+    productImg: emi,
+    type: "99",
 
 },
 { 
     id: '8',
     name: "Nomad Keycaps selection",
-    price: 10,
-    productImg: nomadkc
+    price: 10.00,
+    productImg: nomadkc,
+    type: "99",
 
 },
 { 
     id: '9',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "GMK Amethyst Keyboard",
+    price: 10.00,
+    productImg: gmkam
 
 },
 { 
     id: '10',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "GMK Evil dolch Keyboard",
+    price: 10.00,
+    productImg: gmkevil
 
 },
 { 
     id: '11',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "GMK Rainyday Keyboard",
+    price: 10.00,
+    productImg: gmkrain
 
 },
 { 
     id: '12',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "Kailh Pro Switches",
+    price: 10.00,
+    productImg: kailhp,
+    type: "99",
+
 
 },
 { 
     id: '13',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "Kono Coileder Godox Cable",
+    price: 10.00,
+    productImg: cable
 
 },
 { 
     id: '14',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "Kono Coiled Aviator Cable",
+    price: 10.00,
+    productImg: cable2
 
 },
 { 
     id: '15',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "Sushi Mat",
+    price: 10.00,
+    productImg: sushimat
 
 },
 { 
     id: '16',
-    name: "4",
-    price: 10,
-    productImg: prodImg
+    name: "Discord TKL Keyboard",
+    price: 10.00,
+    productImg: disc
 
 },
 { 
     id: '17',
     name: "4",
-    price: 10,
-    productImg: prodImg
+    price: 10.00,
+    productImg: penguin,
+    type: "99",
+
 
 },
 { 
     id: '18',
     name: "4",
-    price: 10,
-    productImg: prodImg
+    price: 10.00,
+    productImg: starwars
 
 },
+
 ];
